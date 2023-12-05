c190a0cb195c94c120b2a676d2a8ba7a18a117d2
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-05 20:05:31 +0700

    update item controlling

--- src/app/customerControllers/shopSingleController.js
+++ src/app/customerControllers/shopSingleController.js
@@ -204,44 +204,48 @@ class shopSingleController {
           { layout: 'customer/main', title: `Collection ${req.query.date}`, products: multipleMongooseToObject(products), countries: constriesChoice, dates: datesChoice });
         // res.json({ products: multipleMongooseToObject(products) });
       })
+      
       .catch(error => next(error));
   }
 
 
 
   //[GET] /shop-single/:slug
-  async item(req, res, next) {
+  item(req, res, next) {
 
     // res.render('customer/item', { layout: 'customer/main' });
     // console.log(req.params);
-    const thisProduct = await (Product.findOne({slug: req.params.slug }).lean());
-    var related = [];
-    console.log(thisProduct);
+    Product.findOne({slug: req.params.slug }).lean()
+    .then(async thisProduct => {
+      var related = [];
 
-    // console.log(thisProduct);
-    // console.log(reviews);
+      // console.log(thisProduct);
+      // console.log(reviews);
 
-    for (var propt in thisProduct) {
-      if (thisProduct[propt] === true) {
-        related.push(... await Product.find({ [propt]: true, slug: { $ne: thisProduct.slug } }).lean());
-        //console.log(`${propt} : ${product[propt]}`);  
-      }
-    }    
+      for (var propt in thisProduct) {
+        if (thisProduct[propt] === true) {
+          related.push(... await Product.find({ [propt]: true, slug: { $ne: thisProduct.slug } }).lean());
+          //console.log(`${propt} : ${product[propt]}`);  
+        }
+      }    
 
-    const reviews = await (Review.find({product : thisProduct.name}).lean());
+      const reviews = await (Review.find({product : thisProduct.name}).lean());
 
-    // remove duplicate objects
-    const jsonObject = related.map(JSON.stringify);
-    const uniqueSet = new Set(jsonObject);
-    related = Array.from(uniqueSet).map(JSON.parse);
+      // remove duplicate objects
+      const jsonObject = related.map(JSON.stringify);
+      const uniqueSet = new Set(jsonObject);
+      related = Array.from(uniqueSet).map(JSON.parse);
 
-    // limit to 4 products
-    while (related.length > 4) related.pop();
+      // limit to 4 products
+      while (related.length > 4) related.pop();
 
-    res.render('customer/item',
-      { layout: 'customer/main', title: 'Item', product: thisProduct, related : related, reviews: reviews});
-    // res.json({ title: 'All', products: multipleMongooseToObject(products), countries: constriesChoice });
+      res.render('customer/item',
+        { layout: 'customer/main', title: 'Item', product: thisProduct, related : related, reviews: reviews});
+      // res.json({ title: 'All', products: multipleMongooseToObject(products), countries: constriesChoice });
 
+    })
+    .catch(error => next(error));
+    
   }
 
 
