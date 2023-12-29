0a8c55969d301039ebaa18ea3603b8463aba428d
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-29 17:11:42 +0700

    add logout controller

--- src/app/customerControllers/siteController.js
+++ src/app/customerControllers/siteController.js
@@ -6,168 +6,179 @@ require('../../config/passport/passport')(passport);
 
 
 class siteController {
-//[GET] /
-index(req, res) {
-  res.render('customer/home', { layout: 'customer/main' });
-}
+  //[GET] /
+  index(req, res) {
+    res.render('customer/home', { layout: 'customer/main' });
+  }
 
-//[GET] /home
-home(req, res) {
-  res.render('customer/home', { layout: 'customer/main' });
-}
+  //[GET] /home
+  home(req, res) {
+    res.render('customer/home', { layout: 'customer/main' });
+  }
 
-//[GET] /about
-about(req, res) {
-  res.render('customer/about', { layout: 'customer/main' });
-}
+  //[GET] /about
+  about(req, res) {
+    res.render('customer/about', { layout: 'customer/main' });
+  }
 
-//[GET] /elements
-elements(req, res) {
-  res.render('customer/elements', { layout: 'customer/main' });
-}
+  //[GET] /elements
+  elements(req, res) {
+    res.render('customer/elements', { layout: 'customer/main' });
+  }
 
-//[GET] /contact
-contact(req, res) {
-  res.render('customer/contact', { layout: 'customer/main' });
-}
+  //[GET] /contact
+  contact(req, res) {
+    res.render('customer/contact', { layout: 'customer/main' });
+  }
 
-//[GET] /cart
-loginCart(req, res) {
-  res.render('customer/loginCart', { layout: 'customer/main' });
-}
+  //[GET] /cart
+  loginCart(req, res) {
+    res.render('customer/loginCart', { layout: 'customer/main' });
+  }
 
-//[GET] /checkout
-checkout(req, res) {
-  res.render('customer/checkout', { layout: 'customer/main' });
-}
+  //[GET] /checkout
+  checkout(req, res) {
+    res.render('customer/checkout', { layout: 'customer/main' });
+  }
 
-//[GET] /thankyou
-thankyou(req, res) {
-  res.render('customer/thankyou', { layout: 'customer/main' });
-}
+  //[GET] /thankyou
+  thankyou(req, res) {
+    res.render('customer/thankyou', { layout: 'customer/main' });
+  }
 
-//[GET] /login
-login(req, res) {
-  res.render('customer/login', { layout: 'customer/main' });
-}
+  //[GET] /login
+  login(req, res) {
+    res.render('customer/login', { layout: 'customer/main' });
+  }
 
-//[GET] /signup
-signup(req, res) {
-  res.render('customer/signup', { layout: 'customer/main' });
-}
+  //[GET] /signup
+  signup(req, res) {
+    res.render('customer/signup', { layout: 'customer/main' });
+  }
 
-//[POST] /user/store
-signupPost(req, res, next) {}
+  //[POST] /user/store
+  signupPost(req, res, next) {}
 
-//[GET] /user/login
-login(req, res, next){
-  res.render('users/login', { layout: 'customer/main' });
-}
+  //[GET] /user/login
+  login(req, res, next){
+    res.render('users/login', { layout: 'customer/main' });
+  }
 
-// [POST] /user/login
-loginPost(req, res, next) {} 
+  // [POST] /user/login
+  loginPost(req, res, next) {} 
 
-//[GET] /profile/:slug
-profile(req, res, next) {
-  User.findOne({ slug: req.params.slug }).lean()
-    .then(user => {
-      res.render('customer/profile', { layout: 'customer/main', user: user });
-      // res.json({ products: singleMongooseToObject(products) });
-    })
-    .catch(error => next(error));
-}
-
-//[GET] /update-profile/:slug
-updateProfile(req, res, next) {
-  User.findOne({ slug: req.params.slug }).lean()
-    .then(user => {
-      res.render('customer/update-profile', { layout: 'customer/main', user: user });
-      // res.json({ products: singleMongooseToObject(products) });
-    })
-    .catch(error => next(error));
-}
-
-//[POST] /update-profile/:slug
-async update(req, res, next) {
-  const formData = req.body;
-  await User.findOneAndUpdate({ username: formData.username, password: formData.password },
-    {
-      fullname: formData.fullname,
-      email: formData.email,
-      dob: formData.dob,
-      phone: formData.phone,
-      address: formData.address,
-      sex: formData.sex
-    });
-  // res.json({ huhu });
-  res.redirect('/customer/home');
-}
-
-//[POST] /cart/login-success
-loginCartSuccess(req, res, next) {
-  const formData = req.body;
-  User.findOne({ username: formData.username, password: formData.password }).lean()
-    .then(user => {
-      res.redirect(`/customer/cart/${user.slug}`);
-    })
-    .catch(error => next(error));
-}
-
-//[GET] /cart/:slug
-async cart(req, res, next) {
-  let haha = [];
-
-  await User.findOne({ slug: req.params.slug }).lean()
-    .then(async user => {
-      let cartProducts = user.cart;
-
-      let cartWithImg = [];
-      for (var i = 0; i < cartProducts.length; ++i) {
-        let ele = cartProducts[i];
-
-        await Product.findOne({ name: ele.prod }).lean()
-          .then(product => {
-            ele['image'] = product.image;
-          });
-        cartWithImg.push(ele);
+  //[GET] /user/logout
+  logout(req, res, next){
+    console.log("Loging out");
+    req.logout(function(err) {
+      if (err) { 
+        return next(err); 
       }
+      res.redirect('./login');
+  });
+  }
 
-      res.render('customer/cart', { layout: 'customer/main', userFullname: user.fullname, cartWithImg: cartWithImg, userSlug: user.slug })
-      // res.json(haha);
-    })
-    .catch(error => next(error));
-}
+  //[GET] /profile/:slug
+  profile(req, res, next) {
+    User.findOne({ slug: req.params.slug }).lean()
+      .then(user => {
+        res.render('customer/profile', { layout: 'customer/main', user: user });
+        // res.json({ products: singleMongooseToObject(products) });
+      })
+      .catch(error => next(error));
+  }
 
-//[POST] /customer/cart/update-cart/:slug
-async updateCart(req, res, next) {
-  const formData = req.body;
-  const allProducts = Object.keys(formData);
-  const allQuant = Object.values(formData).map(ele => parseFloat(ele));
+  //[GET] /update-profile/:slug
+  updateProfile(req, res, next) {
+    User.findOne({ slug: req.params.slug }).lean()
+      .then(user => {
+        res.render('customer/update-profile', { layout: 'customer/main', user: user });
+        // res.json({ products: singleMongooseToObject(products) });
+      })
+      .catch(error => next(error));
+  }
 
-  if (allProducts.length != 0) {
-    for (var i = 0; i < allProducts.length; ++i) {
-      if (allQuant[i] > 0) {
-        await User.updateOne(
-          { slug: req.params.slug, "cart.prod": allProducts[i] },
-          {
-            $set: {
-              "cart.$.quant": allQuant[i]
+  //[POST] /update-profile/:slug
+  async update(req, res, next) {
+    const formData = req.body;
+    await User.findOneAndUpdate({ username: formData.username, password: formData.password },
+      {
+        fullname: formData.fullname,
+        email: formData.email,
+        dob: formData.dob,
+        phone: formData.phone,
+        address: formData.address,
+        sex: formData.sex
+      });
+    // res.json({ huhu });
+    res.redirect('/customer/home');
+  }
+
+  //[POST] /cart/login-success
+  loginCartSuccess(req, res, next) {
+    const formData = req.body;
+    User.findOne({ username: formData.username, password: formData.password }).lean()
+      .then(user => {
+        res.redirect(`/customer/cart/${user.slug}`);
+      })
+      .catch(error => next(error));
+  }
+
+  //[GET] /cart/:slug
+  async cart(req, res, next) {
+    let haha = [];
+
+    await User.findOne({ slug: req.params.slug }).lean()
+      .then(async user => {
+        let cartProducts = user.cart;
+
+        let cartWithImg = [];
+        for (var i = 0; i < cartProducts.length; ++i) {
+          let ele = cartProducts[i];
+
+          await Product.findOne({ name: ele.prod }).lean()
+            .then(product => {
+              ele['image'] = product.image;
+            });
+          cartWithImg.push(ele);
+        }
+
+        res.render('customer/cart', { layout: 'customer/main', userFullname: user.fullname, cartWithImg: cartWithImg, userSlug: user.slug })
+        // res.json(haha);
+      })
+      .catch(error => next(error));
+  }
+
+  //[POST] /customer/cart/update-cart/:slug
+  async updateCart(req, res, next) {
+    const formData = req.body;
+    const allProducts = Object.keys(formData);
+    const allQuant = Object.values(formData).map(ele => parseFloat(ele));
+
+    if (allProducts.length != 0) {
+      for (var i = 0; i < allProducts.length; ++i) {
+        if (allQuant[i] > 0) {
+          await User.updateOne(
+            { slug: req.params.slug, "cart.prod": allProducts[i] },
+            {
+              $set: {
+                "cart.$.quant": allQuant[i]
+              }
             }
-          }
-        );
-      } else {
-        await User.updateOne(
-          { slug: req.params.slug },
-          {
-            $pull: {
-              'cart': { 'prod': allProducts[i] }
-            }
-          });
+          );
+        } else {
+          await User.updateOne(
+            { slug: req.params.slug },
+            {
+              $pull: {
+                'cart': { 'prod': allProducts[i] }
+              }
+            });
+        }
       }
     }
+    res.redirect('/customer/home');
   }
-  res.redirect('/customer/home');
-}
 }
 
 module.exports = new siteController;
