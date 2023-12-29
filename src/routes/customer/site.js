5e9937e7556d04750f05a281ca863ee6cd47bf8a
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-29 17:35:14 +0700

    add authenticate section

--- src/routes/customer/site.js
+++ src/routes/customer/site.js
@@ -3,16 +3,28 @@ const router = express.Router();
 
 const siteController = require('../../app/customerControllers/siteController');
 
-//Login and update profile
+//Login
 router.use('/login', siteController.login);
-router.post('/login', siteController.loginPost);
+router.post('/login',
+passport.authenticate('local-login', { failureRedirect : './login'}), 
+function(req,res){
+    console.log("redirecting");
+    res.redirect('./protected');
+}, siteController.loginPost);
+
+//Update profile
 router.use('/profile/:slug', siteController.profile);
 router.use('/update-profile/:slug', siteController.updateProfile);
 router.post('/update-profile/updated', siteController.update);
 
 //Signup new profile
 router.use('/signup', siteController.signup);
-router.post('/signup', siteController.signupPost);
+router.post('/signup', 
+passport.authenticate('local-signup', { failureRedirect : './signup'}), 
+function(req,res){
+    console.log("redirecting");
+    res.redirect('./protected');
+}, siteController.signupPost);
 
 //Cart 
 router.use('/cart-login', siteController.loginCart);
@@ -28,5 +40,17 @@ router.use('/elements', siteController.elements);
 router.use('/about', siteController.about);
 router.use('/home', siteController.home);
 router.use('/', siteController.index);
+
+//Route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    console.log("Authenticate checking");
+    if (req.isAuthenticated()){ // is authenticated
+        return next();
+    }
+
+    // is not authenticated
+    res.redirect('/user/login');
+}
 
 module.exports = router;
