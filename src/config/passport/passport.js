685140352a2f888a9067808fba8af9eb963933b2
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-29 16:45:26 +0700

    add module, lib

---
+++ src/config/passport/passport.js
@@ -0,0 +1,7 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../../app/models/User');
+var bcrypt = require('bcrypt'); 
+
+module.exports = function(passport) {
+    
+}
