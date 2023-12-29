9b9f181c831ddbd190f836b632f2fc52ec587b5c
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-29 16:46:21 +0700

    add serialize, deserialize method

--- src/config/passport/passport.js
+++ src/config/passport/passport.js
@@ -3,5 +3,21 @@ var User = require('../../app/models/User');
 var bcrypt = require('bcrypt'); 
 
 module.exports = function(passport) {
-    
+    passport.serializeUser(function(user, done) {
+        console.log("Serializing");
+
+        done(null, user.username);
+    });
+
+    passport.deserializeUser(function(username, done) {
+        User.findByUsername(username).then(function(user) {
+          console.log('deserializing user:',user);
+          done(null, user);
+        }).catch(function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+    });
+
 }
