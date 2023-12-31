ec81c7172d17aa962b767ed62e4b0b331efb22ce
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-31 16:25:42 +0700

    add passport

--- src/routes/admin/site.js
+++ src/routes/admin/site.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+require('../../config/passport/passport')(passport);
 
 const siteController = require('../../app/adminControllers/siteController');
 
