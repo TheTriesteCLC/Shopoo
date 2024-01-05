6321a1b927206d565257de58b61e4436b3bfbf84
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-01-05 16:03:40 +0700

    add add product feature

--- src/app/adminControllers/siteController.js
+++ src/app/adminControllers/siteController.js
@@ -254,9 +254,45 @@ class siteController {
       .catch(error => next(error));
   }
 
+  //[GET] admin/product/add
+  addProduct(req, res, next) {
+    res.render('admin/addProduct', {layout: 'admin/main'});
+  }
+
+  //[POST] admin/product/save
+  saveProduct(req, res, next) {
+    var formData = req.body;
+
+    formData = {
+          'name': formData.name,
+          'price': parseFloat(formData.price),
+          'description': formData.description,
+          'top': 'top' in formData ? true : false,
+          'bottom': 'bottom' in formData ? true : false,
+          'accessories': 'accessories' in formData ? true : false,
+          'outer': 'outer' in formData ? true : false,
+          'shoes': 'shoes' in formData ? true : false,
+          'buyers': parseFloat(formData.buyers),
+          'date': parseFloat(formData.year),
+          'from': formData.from,
+          'stock': parseFloat(formData.stock),
+          'popular': parseFloat(formData.buyers) > 500 ? true : false,
+          'status': formData.stock > 0 ? 'OnStock': 'OutStock'
+        };
+
+    const newProduct = new Product(formData);
+    newProduct.image = "null";
+    newProduct.save();
+    // newProduct
+    console.log(newProduct);
+
+    res.redirect('/admin/tables');
+  }
+
   //[POST] /admin/table/product/update-info/updated
   async updateProductProfileSuccess(req, res, next) {
     const formData = req.body;
+    console.log(formData);
     // const updatedProduct = {
     //   name: formData.name,
     //   price: formData.price,
@@ -293,6 +329,8 @@ class siteController {
         }
       }
     );
+
+    console.log('updated success')
   
     res.redirect('/admin/tables');
   }
